b9ecae54056a7cbb32cb05c6205ac6ed782fe8eb
Werner Hurter (SEB) <Werner.Hurter@sanlam.co.za>
2019-03-06 12:13:18 +0200

    Add create user UI

--- TradeScales.Wpf/ViewModel/Dialogs/AddUserViewModel.cs
+++ TradeScales.Wpf/ViewModel/Dialogs/AddUserViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using TradeScales.Data.Extensions;
 using TradeScales.Data.Infrastructure;
 using TradeScales.Data.Repositories;
 using TradeScales.Entities;
+using TradeScales.Services.Abstract;
 using TradeScales.Wpf.Model;
 using TradeScales.Wpf.Resources.Services.Interfaces;
 using MVVMRelayCommand = TradeScales.Wpf.Model.RelayCommand;
@@ -15,9 +17,9 @@ namespace TradeScales.Wpf.ViewModel.Dialogs
         #region fields
 
         private static IMessageBoxService _messageBoxService = ServiceLocator.Instance.GetService<IMessageBoxService>();
+        private static IMembershipService _membershipService = BootStrapper.Resolve<IMembershipService>();
 
         private IEntityBaseRepository<User> _usersRepository = BootStrapper.Resolve<IEntityBaseRepository<User>>();
-        private IUnitOfWork _unitOfWork = BootStrapper.Resolve<IUnitOfWork>();
 
         #endregion
 
@@ -33,6 +35,39 @@ namespace TradeScales.Wpf.ViewModel.Dialogs
         #region Properties
 
         public bool NotXClosed { get; set; }
+
+        private string _Username;
+        public string Username
+        {
+            get { return _Username; }
+            set
+            {
+                _Username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        private string _Email;
+        public string Email
+        {
+            get { return _Email; }
+            set
+            {
+                _Email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get { return _Password; }
+            set
+            {
+                _Password = value;
+                OnPropertyChanged("Password");
+            }
+        }
 
         #endregion
 
@@ -104,10 +139,26 @@ namespace TradeScales.Wpf.ViewModel.Dialogs
 
         public void Ok()
         {
-            NotXClosed = true;
-            Properties.Settings.Default.Save();
-            MainViewModel.This.ToolOne.CreateNewSerialPort();
-            OnRequestClose();
+            if (_usersRepository.UserExists(_Email, _Username))
+            {
+                throw new ArgumentException("User already exists");
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(_Username) && !string.IsNullOrEmpty(_Email) && !string.IsNullOrEmpty(_Password))
+                {
+
+                    var newUser = _membershipService.CreateUser(_Username, _Email, _Password, new int[] { 2 });
+
+                    Username = "";
+                    Email = "";
+                    Password = "";
+
+                    _messageBoxService.ShowMessageBox($"Successfully added new user {newUser.Username}", "Success", MessageBoxButton.OK);
+                    MainViewModel.This.StatusMessage = $"Successfully added new user {newUser.Username}";
+                    MainViewModel.This.ReloadEntities();
+                }
+            }
         }
 
         private void Cancel()
